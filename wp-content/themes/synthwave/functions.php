1e3cbe1a0daeaea3a852207015f0c6bb923c40bc
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-20 12:05:14 +0100

    Add neon82_enqueue_styles function to functions.php

---
+++ wp-content/themes/synthwave/functions.php
@@ -0,0 +1,11 @@
+
+<?php
+function neon82_enqueue_styles() {
+    // Enregistre et met en file d'attente la feuille de style principale du thème.
+    wp_enqueue_style( 'neon82-style', get_stylesheet_uri() );
+    
+    // Si vous avez d'autres feuilles de style, vous pouvez les ajouter ici.
+    // Par exemple, pour une feuille de style supplémentaire :
+    // wp_enqueue_style( 'neon82-custom-style', get_template_directory_uri() . '/chemin/vers/votre-feuille-de-style.css', array('neon82-style'), '1.0.0' );
+}
+add_action( 'wp_enqueue_scripts', 'neon82_enqueue_styles' );
